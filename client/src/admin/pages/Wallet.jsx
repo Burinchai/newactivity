8712e944269d4601c3717f206318fc84f8056f5a
Burinchai <644230046@webmail.npru.ac.th>
2024-03-19 14:53:10 +0700

    new19-03-67

--- client/src/admin/pages/Wallet.jsx
+++ client/src/admin/pages/Wallet.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ConnectWeb3 from '../components/ConnectWeb3'
-import AddActivity from '../components/addActivityBlock'
 import AddActyivity from '../components/Add_activity'
 import ToContract from '../components/ToContract'
 import Bt from '../contract/bt'
